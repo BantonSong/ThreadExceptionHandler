10bf7fc1dab77a2f680a08c17eb4a7dce740b107
Banton <yawei.song@dianrong.com>
2016-08-15 17:39:59 +0800

    setDefaultUncaughtExceptionHandler和setUncaughtExceptionHandler的区别

--- src/com/test/CrashHandler.java
+++ src/com/test/CrashHandler.java
@@ -3,9 +3,15 @@ package com.test;
 import java.lang.Thread.UncaughtExceptionHandler;
 
 public class CrashHandler implements UncaughtExceptionHandler {
+	private String msg;
+
+	public CrashHandler(String msg) {
+		this.msg = msg;
+	}
 
 	public void uncaughtException(Thread t, Throwable e) {
 		// 打印出异常信息，还可以有其它操作，上传到服务器，保存到本地等等。
+		System.out.println(msg);
 		e.printStackTrace();
 	}
 
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,9 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		MyThread thread = new MyThread();
+		CrashHandler handler1 = new CrashHandler("全局");
 		// 如果没有设置异常处理器，线程中发生未捕获异常，则不知道程序崩溃的原因
-		thread.setUncaughtExceptionHandler(new CrashHandler());
+		// setDefaultUncaughtExceptionHandler方法设置的异常处理器会在线程没有异常处理器的情况下触发
+		Thread.setDefaultUncaughtExceptionHandler(handler1);
+		// 也可以调用setUncaughtExceptionHandler方法，只有在当前Thread发生异常时才会被处理
+		CrashHandler handler2 = new CrashHandler("局部");
+		thread.setUncaughtExceptionHandler(handler2);// 发生异常会输出“局部”
 		thread.start();
 	}
-
 }
